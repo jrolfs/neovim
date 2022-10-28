8c9cf2f650672f7122098162dd84edd59e4f9d35
Jamie Rolfs <jamie.rolfs@hover.to>
2022-10-28 12:19:25 -0700

    fix: configure Catppuccin correctly, drop defaults in there

--- home/.config/nvim/lua/plugins/interface/catppuccin.lua
+++ home/.config/nvim/lua/plugins/interface/catppuccin.lua
@@ -1,13 +1,38 @@
 require("catppuccin").setup {
-  flavour = "frappe",
-  transparent_background = true
+    flavour = "frappe",
+    background = {
+        light = "latte",
+        dark = "frappe",
+    },
+    compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+    transparent_background = true,
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+    },
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = false,
+        telescope = true,
+        treesitter = true,
+    },
 }
-
-vim.api.nvim_create_autocmd("OptionSet", {
-  pattern = "background",
-  callback = function()
-    if vim.g.colors_name == 'catppuccin' then
-      vim.cmd("Catppuccin " .. (vim.v.option_new == "light" and "latte" or "frappe"))
-    end
-  end,
-})
