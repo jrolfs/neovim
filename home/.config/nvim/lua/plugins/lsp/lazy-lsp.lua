051ec2b83699d91e29f9bdf4a8b60b874448c768
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-10-29 00:42:41 -0700

    feat: add mappings for lsp

--- home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
+++ home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
@@ -1,5 +1,9 @@
+local config = require('plugins.lsp.nvim-lspconfig')
+
 require('lazy-lsp').setup {
   excluded_servers = { 'denols' },
-  default_config = {},
+  default_config = {
+    on_attach = config.on_attach
+  },
   configs = {},
 }
---
+++ home/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -0,0 +1,35 @@
+local options = { noremap=true, silent=true }
+
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, options)
+vim.keymap.set('n', '[d',        vim.diagnostic.goto_prev,  options)
+vim.keymap.set('n', ']d',        vim.diagnostic.goto_next,  options)
+vim.keymap.set('n', '<c-e> m',   vim.diagnostic.goto_next,  options)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, options)
+
+--- This belongs here because `on_attach` is a native LSP thing,
+--  but we export it because `lazy-lsp` makes it easier to apply
+--  to every language server instead of having to manually do so
+local on_attach = function(_, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+return { on_attach = on_attach }
