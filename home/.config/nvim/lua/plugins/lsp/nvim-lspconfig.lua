5588ee4358b43541b6a183e9513f8ca967a1a1ba
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-10-29 03:52:18 -0700

    feat: familiar mapping for formatting

--- home/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -29,7 +29,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', '<leader>af', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 return { on_attach = on_attach }
