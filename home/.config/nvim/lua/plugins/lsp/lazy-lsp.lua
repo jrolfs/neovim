de3fccbd45fd4f720d5b911701ca954c6fda1314
Jamie Rolfs <jamie.rolfs@hover.to>
2022-11-02 12:43:08 -0700

    fix(lazy-lsp): disable obsolete `rls` LSP for Rust

--- home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
+++ home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
@@ -1,7 +1,10 @@
 local config = require('plugins.lsp.nvim-lspconfig')
 
 require('lazy-lsp').setup {
-  excluded_servers = { 'denols' },
+  excluded_servers = {
+    'denols', -- TypeScript: if I ever start using Deno, I'll need to figure out how to load this selectively
+    'rls', -- Rust: no longer supported (prefer `rust-analyzer`
+  },
   default_config = {
     on_attach = config.on_attach
   },
