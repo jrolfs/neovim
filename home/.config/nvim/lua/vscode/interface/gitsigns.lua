be84333e031e3f2b6ed6331e773131f21e4678cc
Jamie Rolfs <jamie.rolfs@hover.to>
2023-03-29 11:23:11 -0700

    feat(vscode): add binding for Git range reset

--- home/.config/nvim/lua/vscode/interface/gitsigns.lua
+++ home/.config/nvim/lua/vscode/interface/gitsigns.lua
@@ -5,7 +5,7 @@ map('n', '[c', 'workbench.action.editor.previousChange')
 map('n', ']c', 'workbench.action.editor.nextChange')
 
 -- map({'n','v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
--- map({'n','v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+map({'n','v'}, '<leader>hr', 'git.revertSelectedRanges')
 -- map('n',       '<leader>hS', gs.stage_buffer)
 -- map('n',       '<leader>hu', gs.undo_stage_hunk)
 -- map('n',       '<leader>hR', gs.reset_buffer)
