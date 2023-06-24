06186484c44c01edc544ae2015ed7fef0bb6f556
Jamie Rolfs <jamie.rolfs@hover.to>
2023-06-24 15:27:35 -0700

    fix: disable sqls language server

--- home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
+++ home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
@@ -4,7 +4,8 @@ require('lazy-lsp').setup {
   excluded_servers = {
     'denols', -- TypeScript: if I ever start using Deno, I'll need to figure out how to load this selectively
     'rls', -- Rust: no longer supported (prefer `rust-analyzer`
-    'rust_analyzer' -- Letting 'rust-tools.nvim' manage this
+    'rust_analyzer', -- Letting 'rust-tools.nvim' manage this
+    'sqls' -- Deprecated (prefer `sqlls`)
   },
   default_config = {
     on_attach = config.on_attach
