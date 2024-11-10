2a96edccd21080487796eb63f6ca01ccfba452a7
Jamie Rolfs <jamie.rolfs@gmail.com>
2024-11-10 00:31:28 -0800

    fix(lazy-lsp): disable solargraph

--- home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
+++ home/.config/nvim/lua/plugins/lsp/lazy-lsp.lua
@@ -2,15 +2,17 @@ local config = require('plugins.lsp.nvim-lspconfig')
 
 require('lazy-lsp').setup {
   excluded_servers = {
-    'denols', -- TypeScript: if I ever start using Deno, I'll need to figure out how to load this selectively
-    'rls', -- Rust: no longer supported (prefer `rust-analyzer`
+    'denols',        -- TypeScript: if I ever start using Deno, I'll need to figure out how to load this selectively
+    'rls',           -- Rust: no longer supported (prefer `rust-analyzer`
     'rust_analyzer', -- Letting 'rust-tools.nvim' manage this
-    'sqls', -- Deprecated (prefer `sqlls`)
+    'sqls',          -- Deprecated (prefer `sqlls`)
     -- Getting errors for these after latest nvim plugin update
     'dolmenls',
     'jqls',
     -- Getting errors for these after latest nvim plugin update (part 2)
-    'jdtls'
+    'jdtls',
+    -- Prefer Ruby LSP + standardrb
+    'solargraph'
   },
   default_config = {
     on_attach = config.on_attach
