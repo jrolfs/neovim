f0367670b1c4b63c3df7638c0e5e08c657bff035
Jamie Rolfs <jamie.rolfs@gmail.com>
2025-09-19 16:53:28 -0700

    fix: hopefully make comment wrapping work in JS/TS?

--- home/.config/nvim/lua/init.lua
+++ home/.config/nvim/lua/init.lua
@@ -16,3 +16,11 @@ require('relative-source').source { home .. '/plugins/*/*.lua' }
 
 vim.cmd.colorscheme(vim.g.colorscheme)
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"javascript", "typescript"},
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append("croql")
+    vim.opt_local.comments = "sO:* -,mO:*  ,exO:*/,s1:/*,mb:*,ex:*/,://"
+  end,
+})
