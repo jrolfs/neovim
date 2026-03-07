64b91c8ba87e8d696c90b100c4df0b085c6a4ca6
Jamie Rolfs <jamie.rolfs@gmail.com>
2026-03-06 23:49:48 -0800

    fix(nvim-treesitter): update for 0.11

--- home/.config/nvim/lua/plugins/syntax/nvim-treesitter.lua
+++ home/.config/nvim/lua/plugins/syntax/nvim-treesitter.lua
@@ -1,8 +1,4 @@
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-
-  sync_install = false,
+require('nvim-treesitter').setup {
+  ensure_installed = 'all',
   auto_install = true,
-
-  highlight = { enable = true },
 }
