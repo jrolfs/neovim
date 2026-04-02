7f6a823655f00c9c73768ee001f316c3ee8748d6
Jamie Rolfs <jamie.rolfs@gmail.com>
2026-04-01 17:30:33 -0700

    fixup! feat(lsp): manually configure oxlint and oxfmt

--- home/.config/nvim/lua/plugins/lsp/oxlint.lua
+++ home/.config/nvim/lua/plugins/lsp/oxlint.lua
@@ -1,22 +1,15 @@
 local config = require('plugins.lsp.nvim-lspconfig')
 
-local function log(msg)
-  vim.notify('[oxlint] ' .. msg, vim.log.levels.INFO)
-end
-
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   callback = function(args)
     local fname = vim.api.nvim_buf_get_name(args.buf)
     local dir = vim.fs.dirname(fname)
-    log('FileType fired for ' .. fname .. ' (dir: ' .. dir .. ')')
 
     local rc = vim.fs.find('.oxlintrc.json', { path = dir, upward = true })[1]
     if not rc then
-      log('No .oxlintrc.json found from ' .. dir)
       return
     end
-    log('Found rc: ' .. rc)
 
     local root = vim.fs.dirname(rc)
 
@@ -25,10 +18,8 @@ vim.api.nvim_create_autocmd('FileType', {
       bin = 'oxlint'
     end
     if not bin then
-      log('No oxlint binary found (checked node_modules from ' .. root .. ' and PATH)')
       return
     end
-    log('Using binary: ' .. bin .. ', root: ' .. root)
 
     vim.lsp.start({
       name = 'oxlint',
@@ -36,8 +27,5 @@ vim.api.nvim_create_autocmd('FileType', {
       root_dir = root,
       on_attach = config.on_attach,
     })
-    log('vim.lsp.start() called')
   end,
 })
-
-log('autocmd registered')
